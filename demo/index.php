d136995188c9e79dfb20a69750ee632d18c36345
wang-arshton256 <wangarshton@gmail.com>
2025-03-12 03:51:44 +0300

    echoing hello world

---
+++ demo/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>DEmo</title>
+</head>
+<body>
+    
+
+
+    <h1>
+        <?php 
+        echo "Hello, World";
+        ?>
+    </h1>
+</body>
+</html>
